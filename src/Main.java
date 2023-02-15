c6e52aea0c5f11374699f904c2396a41dc3fbaa5
Василий <remindscope@gmail.com>
2023-02-15 21:52:45 +0300

    streams

--- src/Main.java
+++ src/Main.java
@@ -1,93 +1,25 @@
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        class Human {
-            String name;
+        Stream<String> stream = Stream.of("A", "AA", "BB", "C", "XXX");
+        Comparator<String> comparator = Comparator.comparing(String::length);
+        BiConsumer<String, String> consumer = (x, y) -> System.out.println(x + "\n" + y);
 
-            public Human(String name) {
-                this.name = name;
-            }
+        findMinMax(stream, comparator, consumer);
+        findMinMax(Stream.empty(), comparator, consumer);
 
-            @Override
-            public String toString() {
-                return name;
-            }
-        }
-
-        Predicate<Integer> predicate1 = new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer integer) {
-                return integer > 0;
-            }
-        };
-        System.out.println(predicate1.test(1));
-
-        Predicate<Integer> predicate2 = x -> x > 0;
-
-        System.out.println(predicate2.test(-2));
-
-        Human man = new Human("Dude");
-        Human woman = new Human("Girl");
-
-        Consumer<Human> consumer1 = new Consumer<Human>() {
-            @Override
-            public void accept(Human human) {
-                System.out.println("hi " + human);
-            }
-        };
-        consumer1.accept(man);
-
-        Consumer<Human> consumer2 = x -> System.out.println("hi " + x);
-        consumer2.accept(woman);
-
-        Function<Double, Long> function1 = new Function<Double, Long>() {
-            @Override
-            public Long apply(Double aDouble) {
-                return aDouble.longValue();
-            }
-        };
-        Function<Double, Long> function2 = x -> Math.round(x);
-
-        Supplier<Integer> supplier1 = new Supplier<Integer>() {
-            @Override
-            public Integer get() {
-                return (int) (Math.random() * 100);
-            }
-        };
-        System.out.println(supplier1.get());
-
-        Supplier<Integer> supplier2 = () -> (int) (Math.random() * 100);
-
-        System.out.println(supplier2.get());
-
-        Predicate<Human> condition = h -> h.name.contains("a");
-        Function<Human, String> ifTrue = Object::toString;
-        Function<Human, String> ifFalse = s -> "False";
-
-        System.out.println(ternaryOperator(condition, ifTrue, ifFalse).apply(man));
-
-
-        Supplier<Human> supHuman = () -> new Human(Character.getName(supplier2.get()));
-
-        Human dan = new Human("Dan");
-        Consumer<Human> cons = h -> System.out.println(ternaryOperator(condition, ifTrue, ifFalse).apply(h));
-        cons.accept(supHuman.get());
-        cons.accept(dan);
-
+        double res = Stream.generate(new Random()::nextInt).limit(20).filter(x -> x % 2 ==0).peek(System.out::println).count();
+        System.out.println((int)res + " even numbers");
     }
 
-    public static <T, U> Function<T, U> ternaryOperator(
-            Predicate<? super T> condition,
-            Function<? super T, ? extends U> ifTrue,
-            Function<? super T, ? extends U> ifFalse) {
-        return x -> condition.test(x) ? ifTrue.apply(x) : ifFalse.apply(x);
+    public static <T> void findMinMax(Stream<? extends T> stream, Comparator<? super T> order, BiConsumer<? super T, ? super T> consumer) {
+        Deque<? extends T> q = stream.sorted(order).collect(Collectors.toCollection(LinkedList::new));
+        consumer.accept(q.peekFirst(), q.peekLast());
     }
-
-
 }
 
 
